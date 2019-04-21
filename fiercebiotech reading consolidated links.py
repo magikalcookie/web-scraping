6ff2024667a9f8de2468ca6b33ab5000893b41c8
magikalcookie <44452912+magikalcookie@users.noreply.github.com>
2019-04-21 09:59:14 -0700

    Update fiercebiotech reading consolidated links.py

--- fiercebiotech reading consolidated links.py
+++ fiercebiotech reading consolidated links.py
@@ -25,39 +25,38 @@ count_list = []
     ##a = content[int(y)]
 ##If doing it all at once, use: 
     ##for a in content:
-for a in content:
-    for i in a:
-        dash = i.find('-')
-        if dash == -1:
-            pass
-            count+=1
-        else:
-            count_list.append(count)
-    #print count_list
-    
-    #Append first term (minus '/')
-    first = a[1:count_list[0]+0]
-    #print first
-    if first not in final_dict:
-        final_dict[first] = 1
+for i in a:
+    dash = i.find('-')
+    if dash == -1:
+        pass
+        count+=1
     else:
-        final_dict[first] += 1
-    
-    #Loop for words in between first and last
-    for i in range(len(count_list)):
-        mid = a[count_list[i-1]+i:count_list[i]+i]
-        #print mid
-        if mid not in final_dict:
-            final_dict[mid] = 1
-        else:
-            final_dict[mid] += 1
-    #Append last term
-    d = len(count_list)
-    last = a[count_list[d-1]+d:len(a)]
-    #print last
-    if last not in final_dict:
-        final_dict[last] = 1
+        count_list.append(count)
+#print count_list
+
+#Append first term (minus '/')
+first = a[1:count_list[0]+0]
+#print first
+if first not in final_dict:
+    final_dict[first] = 1
+else:
+    final_dict[first] += 1
+  
+#Loop for words in between first and last
+for i in range(len(count_list)):
+    mid = a[count_list[i-1]+i:count_list[i]+i]
+    #print mid
+    if mid not in final_dict:
+        final_dict[mid] = 1
     else:
-        final_dict[last] += 1
+        final_dict[mid] += 1
+#Append last term
+d = len(count_list)
+last = a[count_list[d-1]+d:len(a)]
+#print last
+if last not in final_dict:
+    final_dict[last] = 1
+else:
+    final_dict[last] += 1
     
-    print final_dict
+print final_dict
